50e9e761190dd2ebd971fc24546408e76f29e62d
unknown <danielson7632@gmail.com>
2026-01-06 11:32:41 +0700

    Connect to postgresql database

--- Backend/cmd/main.go
+++ Backend/cmd/main.go
@@ -4,10 +4,22 @@ import (
 	"log"
 
 	"github.com/DanielsonNg/SpendingTracker/tree/main/Backend/cmd/api"
+	"github.com/DanielsonNg/SpendingTracker/tree/main/Backend/internal/database"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+	if err := godotenv.Load(); err != nil {
+		log.Println("Environment variable not found")
+	}
+	db, err := database.NewPostgres()
+	if err != nil {
+		log.Fatal("Database Connection Failed", err)
+	}
+	defer db.Close()
+
+	log.Println("Connected to database")
+	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
---
+++ Backend/internal/database/postgres.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func NewPostgres() (*sql.DB, error) {
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_SSLMODE"),
+	)
+
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(25)
+
+	return db, nil
+}
