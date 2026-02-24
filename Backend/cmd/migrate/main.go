c8ac9e6bc152f04dfe1da36f680548f19ea11140
Danielson Ng <danielson7632@gmail.com>
2026-02-24 11:13:42 +0700

    Migrate Progress

--- Backend/cmd/migrate/main.go
+++ Backend/cmd/migrate/main.go
@@ -1,1 +1,39 @@
 package migrate
+
+import (
+	"log"
+	"os"
+
+	"github.com/DanielsonNg/SpendingTracker/tree/main/Backend/internal/database"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("Environment variable not found")
+	}
+	db, err := database.NewPostgres()
+	if err != nil {
+		log.Fatal("Database Connection Failed", err)
+	}
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	m, err := migrate.NewWithDatabaseInstance(
+		"file:///migrations",
+		"postgres", driver)
+
+	cmd := os.Args[(len(os.Args) - 1)]
+	if cmd == "up" {
+		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	}
+	if cmd == "down" {
+		if err := m.Down(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	}
+
+	// m.Up() // or m.Steps(2)
+}
